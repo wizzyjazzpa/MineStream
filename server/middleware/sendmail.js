0b6798baa2c5c959e37b0ddab43c04295429ab26
OGM <walisonmichael8@gmail.com>
2025-10-15 15:33:42 +0100

    mailjet

--- server/middleware/sendmail.js
+++ server/middleware/sendmail.js
@@ -2,14 +2,13 @@ require('dotenv').config();
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for 465, false for 587
-    auth:{
-        user:process.env.EMAIL,
-        pass:process.env.PASSWORD
-    }
+ host: "in-v3.mailjet.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.MAILJET_API_KEY,
+    pass: process.env.MAILJET_SECRET_KEY,
+  },
 
 });
 
