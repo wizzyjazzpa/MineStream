97a2a50bd388304c404ce0cb8e845fc5cabc4514
OGM <walisonmichael8@gmail.com>
2025-10-15 14:40:07 +0100

    sendmail

--- server/middleware/sendmail.js
+++ server/middleware/sendmail.js
@@ -3,7 +3,9 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
 
-    service:'gmail',
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for 587
     auth:{
         user:process.env.EMAIL,
         pass:process.env.PASSWORD
